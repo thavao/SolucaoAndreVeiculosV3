a096dd9e0657e1cee787e22b7bb1c2ca8900e5cc
thavao <othavioact@gmail.com>
2024-06-11 17:20:50 -0300

    Add: Novo projeto Geral para gerar migration com todas as tabelas necesssárias de uma única vez

---
+++ 01_ApiAndreVeiculos_Geral/Data/_01_ApiAndreVeiculos_GeralContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Models;
+
+namespace _01_ApiAndreVeiculos_Geral.Data
+{
+    public class _01_ApiAndreVeiculos_GeralContext : DbContext
+    {
+        public _01_ApiAndreVeiculos_GeralContext (DbContextOptions<_01_ApiAndreVeiculos_GeralContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Models.TermoDeUso> TermoDeUso { get; set; } = default!;
+
+    }
+}
