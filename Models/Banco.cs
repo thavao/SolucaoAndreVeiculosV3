b811c4aa10281032dc719cb7bc05004d08ab0192
thavao <othavioact@gmail.com>
2024-06-11 17:18:45 -0300

    Add: Novas classes

---
+++ Models/Banco.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Models
+{
+    public class Banco
+    {
+        [Key]
+        public string CNPJ { get; set; }
+        public string NomeBanco { get; set; }
+        public DateTime DataFundacao { get; set; }
+    }
+}
---
+++ Models/Financiamento.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class Financiamento
+    {
+        public int Id { get; set; }
+        public Venda venda { get; set; }
+        public DateTime DataFinanciamento { get; set; }
+        public Banco Banco { get; set; }
+    }
+}
