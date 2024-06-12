e322946709666cd6004d5d224f5ad066ad9eef1b
thavao <othavioact@gmail.com>
2024-06-12 16:53:39 -0300

    Add: Migration geral

--- 01_ApiAndreVeiculos_Geral/Data/_01_ApiAndreVeiculos_GeralContext.cs
+++ 01_ApiAndreVeiculos_Geral/Data/_01_ApiAndreVeiculos_GeralContext.cs
@@ -14,7 +14,32 @@ namespace _01_ApiAndreVeiculos_Geral.Data
         {
         }
 
+        public DbSet<Models.AceiteTermoDeUso> AceiteTermoDeUso { get; set; } = default!;
+        public DbSet<Models.Banco> Banco { get; set; } = default!;
+        public DbSet<Models.Boleto> Boleto { get; set; } = default!;
+        public DbSet<Models.Cargo> Cargo { get; set; } = default!;
+        public DbSet<Models.Carro> Carro { get; set; } = default!;
+        public DbSet<Models.CarroServico> CarroServico { get; set; } = default!;
+        public DbSet<Models.Cartao> Cartao { get; set; } = default!;
+        public DbSet<Models.Categoria> Categoria { get; set; } = default!;
+        public DbSet<Models.Cliente> Cliente { get; set; } = default!;
+        public DbSet<Models.CNH> CNH { get; set; } = default!;
+        public DbSet<Models.Compra> Compra { get; set; } = default!;
+        public DbSet<Models.Condutor> Condutor { get; set; } = default!;
+        public DbSet<Models.Dependente> Dependente { get; set; } = default!;
+        public DbSet<Models.Endereco> Endereco { get; set; } = default!;
+        public DbSet<Models.Financiamento> Financiamento { get; set; } = default!;
+        public DbSet<Models.Funcionario> Funcionario { get; set; } = default!;
+        public DbSet<Models.Pagamento> Pagamento { get; set; } = default!;
+        public DbSet<Models.PendenciaFinanceira> PendenciaFinanceira { get; set; } = default!;
+        public DbSet<Models.Pix> Pix { get; set; } = default!;
+        public DbSet<Models.Seguro> Seguro { get; set; } = default!;
+        public DbSet<Models.Servico> Servico { get; set; } = default!;
         public DbSet<Models.TermoDeUso> TermoDeUso { get; set; } = default!;
+        public DbSet<Models.TipoPix> TipoPix { get; set; } = default!;
+        public DbSet<Models.Venda> Venda { get; set; } = default!;
+
+
 
     }
 }
