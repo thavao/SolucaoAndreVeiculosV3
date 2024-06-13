ae74e1c2b8571aa4ec54e2710d87590e9ce69a57
thavao <othavioact@gmail.com>
2024-06-13 19:43:17 -0300

    Add: Classe generica para services em Mongo

--- Models/Banco.cs
+++ Models/Banco.cs
@@ -1,10 +1,13 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Models.Interfaces;
+using MongoDB.Bson.Serialization.Attributes;
+using System.ComponentModel.DataAnnotations;
 
 namespace Models
 {
-    public class Banco
+    public class Banco 
     {
         [Key]
+        [BsonId]
         public string CNPJ { get; set; }
         public string NomeBanco { get; set; }
         public DateTime DataFundacao { get; set; }
---
+++ Services/GeralServiceMongoDb.cs
@@ -0,0 +1,42 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public class GeralServiceMongoDb<T>
+    {
+        private readonly IMongoCollection<T> _colecao;
+        private readonly string _connectionString = "mongodb://root:Mongo%402024%23@localhost:27017/";
+        private readonly string _dataBaseName = "AndreVeiculos";
+
+        public GeralServiceMongoDb(string collectionName)
+        {
+            var client = new MongoClient(_connectionString);
+            var dataBase = client.GetDatabase(_dataBaseName);
+            _colecao = dataBase.GetCollection<T>(collectionName);
+        }
+        public List<T> Get() => _colecao.Find(T => true).ToList();
+        public T Get(string id) => _colecao.Find(Builders<T>.Filter.Eq("_id", new ObjectId(id))).FirstOrDefault();
+
+        public T Create(T item)
+        {
+            _colecao.InsertOne(item);
+            return item;
+        }
+        public void Update(T item, string id)
+        {
+            _colecao.ReplaceOne(Builders<T>.Filter.Eq("_id", new ObjectId(id)), item);
+        }
+
+        public void Remove(string id)
+        {
+            _colecao.DeleteOne(Builders<T>.Filter.Eq("_id", new ObjectId(id)));
+        }
+
+    }
+}
