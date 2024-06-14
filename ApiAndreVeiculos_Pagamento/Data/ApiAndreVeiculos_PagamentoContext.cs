13db0c4d2d0b8314c8bee00fd60597744875f689
thavao <othavioact@gmail.com>
2024-06-14 15:56:18 -0300

    feat: api de pagamento

---
+++ ApiAndreVeiculos_Pagamento/Data/ApiAndreVeiculos_PagamentoContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Models;
+
+namespace ApiAndreVeiculos_Pagamento.Data
+{
+    public class ApiAndreVeiculos_PagamentoContext : DbContext
+    {
+        public ApiAndreVeiculos_PagamentoContext(DbContextOptions<ApiAndreVeiculos_PagamentoContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Models.Pagamento> Pagamento { get; set; } = default!;
+        public DbSet<Models.Pix> Pix { get; set; } = default!;
+        public DbSet<Models.Cartao> Cartao { get; set; } = default!;
+        public DbSet<Models.Boleto> Boleto { get; set; } = default!;
+
+    }
+}
