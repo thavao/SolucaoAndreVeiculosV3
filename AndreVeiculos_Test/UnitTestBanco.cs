9171af3f965b32c1dd315212fb0033416d0f1ee4
thavao <othavioact@gmail.com>
2024-06-14 00:37:06 -0300

    test: teste unitario de post para Bancos

---
+++ AndreVeiculos_Test/UnitTestBanco.cs
@@ -0,0 +1,34 @@
+﻿using ApiAndreVeiculos_Banco.Controllers;
+using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AndreVeiculos_Test
+{
+    public class UnitTestBanco
+    {
+        [Fact]
+        public void TestPost()
+        {
+            BancosController controller = new(new ConnectionFactory());
+
+            for (int i = 0; i < 99999; i++)
+            {
+                controller.PostMQBanco(
+                    new Models.Banco()
+                    {
+                        CNPJ = "Teste " + new Random().Next(1, 100000),
+                        NomeBanco = "Banco " + new Random().Next(1, 100000),
+                        DataFundacao = DateTime.Now
+                    }
+                );
+
+                Assert.True( true );
+            }
+        }
+    }
+}
+
