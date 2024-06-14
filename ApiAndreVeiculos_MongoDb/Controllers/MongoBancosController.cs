55f7993d802688fc328e32d3748ab48994436f6e
thavao <othavioact@gmail.com>
2024-06-14 11:48:56 -0300

    add: Api para inserir dados no mongo

---
+++ ApiAndreVeiculos_MongoDb/Controllers/MongoBancosController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Models;
+using Services;
+
+namespace ApiAndreVeiculos_MongoDb.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MongoBancosController : ControllerBase
+    {
+        private readonly GeralServiceMongoDb<Banco> _mongoDb;
+        readonly string colecao = "Banco";
+        public MongoBancosController()
+        {
+            _mongoDb = new GeralServiceMongoDb<Banco>(colecao);
+        }
+
+        [HttpPost]
+        public async Task<Banco> Post(Banco banco)
+        {
+            return _mongoDb.Create(banco);
+        }
+    }
+}
